4a00b88d0e2a8fadaeb1e0ba6831af44d27708f9
Viennarz Curtiz <viennarzc@gmail.com>
2023-02-24 08:17:49 +0800

    create project

---
+++ AirpodsProLikeAnimation/AirpodsProLikeAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirpodsProLikeAnimationApp.swift
+//  AirpodsProLikeAnimation
+//
+//  Created by Viennarz Curtiz on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct AirpodsProLikeAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirpodsProLikeAnimation/ContentView.swift
@@ -0,0 +1,104 @@
+//
+//  ContentView.swift
+//  AirpodsProLikeAnimation
+//
+//  Created by Viennarz Curtiz on 2/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    enum Mode {
+        case noiseCancellation
+        case transparency
+        case off
+    }
+    
+    @Namespace private var circleAnimate
+    
+    @State private var selectedMode: Mode = .off
+    
+    var body: some View {
+        VStack {
+            HStack {
+                ZStack {
+                    if selectedMode == .noiseCancellation {
+                        Circle()
+                            .fill(Color.blue)
+                            .frame(width: 60, height: 60)
+                            .matchedGeometryEffect(id: "selection", in: circleAnimate)
+                        
+                    }
+                    
+                    Image(systemName:
+                            "person.fill")
+                        .foregroundColor(.white)
+                        .padding()
+                }
+                .onTapGesture {
+                    withAnimation {
+                        selectedMode = .noiseCancellation
+                        
+                    }
+                }
+                
+                Spacer()
+                
+                ZStack {
+                    if selectedMode == .off {
+                        Circle()
+                            .fill(Color.blue)
+                            .frame(width: 60, height: 60)
+                            .matchedGeometryEffect(id: "selection", in: circleAnimate)
+                        
+                    }
+                    
+                    Image(systemName: "person.fill")
+                        .foregroundColor(.white)
+                        .padding()
+                }
+                .onTapGesture {
+                    withAnimation {
+                        selectedMode = .off
+                        
+                    }
+                }
+                
+                
+                Spacer()
+                
+                ZStack {
+                    if selectedMode == .transparency {
+                        Circle()
+                            .fill(Color.blue)
+                            .frame(width: 60, height: 60)
+                            .matchedGeometryEffect(id: "selection", in: circleAnimate)
+                        
+                    }
+                    
+                    Image(systemName: "person.fill")
+                        .foregroundColor(.white)
+                        .padding()
+                }
+                .onTapGesture {
+                    withAnimation {
+                        selectedMode = .transparency
+                    }
+                }
+                
+            }
+            .padding(4)
+            .frame(width: 300)
+            .background(Capsule())
+            .font(.title2)
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
